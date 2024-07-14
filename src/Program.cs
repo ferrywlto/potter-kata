70978c7fbe620a8cc3c788f680a2064f0f2cc189
Ferry To <ferrywlto@gmail.com>
2024-07-14 14:55:29 +0100

    feat: Link the class with console ui

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,39 @@
-﻿Console.WriteLine("Welcome to Potter Kata book store!");
-
-const string inputMessage = "Add a book to cart (input 'exit' to finish): ";
-var cart = new BookShoppingCart();
-
-string? input;
-do
+﻿using System.Diagnostics.CodeAnalysis;
+[ExcludeFromCodeCoverage]
+public partial class Program
 {
-    Console.Write(inputMessage);
-}
-while ((input = Console.ReadLine()?.ToLower()) != "exit");
+    const string exitCommand = "exit";
+    const string inputMessage = "Add a book to cart (input 'exit' to finish): ";
+    const string welcomeMessage = "Welcome to Potter Kata book store!";
+    const string invalidInputMessage = "Invalid input, please enter again";
+    const string resultMessage = "The total price of the books is: €";
 
-Console.WriteLine($"The total amount of the cart is: €{10}");
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(welcomeMessage);
+
+        var cart = new BookShoppingCart();
+
+        string? input;
+        while (true)
+        {
+            Console.Write(inputMessage);
+            input = Console.ReadLine()?.ToLower();
+            
+            if(string.IsNullOrEmpty(input)) 
+            {
+                Console.WriteLine(invalidInputMessage);
+                continue;
+            }
+            if (input.Equals(exitCommand)) break;
+            
+            if(!(int.TryParse(input, out var value) && cart.Add(value))) 
+            {
+                Console.WriteLine(invalidInputMessage);
+                continue;
+            }
+        }
+        Console.Write(resultMessage);
+        Console.WriteLine(Math.Round(cart.Calculate(),2));
+    }
+}

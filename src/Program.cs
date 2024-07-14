66d51f089ade50a6276bd3af2a44f074985b7a16
Ferry To <ferrywlto@gmail.com>
2024-07-14 10:59:01 +0100

    ui: Setup console input interface

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Potter Kata!");
+﻿Console.WriteLine("Welcome to Potter Kata book store!");
+
+const string inputMessage = "Add a book to cart (input 'exit' to finish): ";
+var cart = new BookShoppingCart();
+
+string? input;
+do
+{
+    Console.Write(inputMessage);
+}
+while ((input = Console.ReadLine()?.ToLower()) != "exit");
+
+Console.WriteLine($"The total of the cart is: £{10}");

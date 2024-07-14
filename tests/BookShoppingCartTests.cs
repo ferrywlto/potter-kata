e8e3fbddc285465f3050ea66a0eb204dbbe3d273
Ferry To <ferrywlto@gmail.com>
2024-07-14 12:05:19 +0100

    test: Add valid book id input tests

--- tests/BookShoppingCartTests.cs
+++ tests/BookShoppingCartTests.cs
@@ -1,3 +1,35 @@
 namespace PotterKata.UnitTests;
 
-public class BookShoppingCartTests {}
+public class BookShoppingCartTests 
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Add_OnIdLessThanOne_ReturnFalse(int bookId)
+    {
+        var sut = new BookShoppingCart();
+        var actual = sut.Add(bookId);
+        Assert.False(actual);
+    }
+
+    [Theory]
+    [InlineData(6)]
+    [InlineData(10)]
+    public void Add_OnIdLargerThanFive_ReturnFalse(int bookId)
+    {
+        var sut = new BookShoppingCart();
+        var actual = sut.Add(bookId);
+        Assert.False(actual);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(3)]
+    [InlineData(5)]
+    public void Add_OnBookIdBetweenOneToFive_ReturnTrue(int bookId)
+    {
+        var sut = new BookShoppingCart();
+        var actual = sut.Add(bookId);
+        Assert.True(actual);
+    }
+}
